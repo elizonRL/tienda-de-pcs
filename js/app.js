ef94fcb5c11c151b521343fac63cbdd7c2340526
elizo <elizo@DESKTOP-0J85MTD>
2023-03-17 16:21:56 -0400

    mejorando el css

--- js/app.js
+++ js/app.js
@@ -1,23 +1,29 @@
 var imgProducto;
 var itemImg; 
+var arrayItem = [];
 var avatarCont;
+var content;
 function moviendoImg(event){
     itemImg = event.target;
-    console.log(itemImg);
+    arrayItem.push(itemImg.src);
+    console.log(arrayItem);
 }
+
 function cambiarImg(event){
-    avatarCont.src= itemImg.src;
-    console.log(avatarCont);
+    for(let itenArray of arrayItem){
+      content.innerHTML = content.innerHTML +`<img src='${itenArray}'>`
+    }
 }
+
 function domCargado(){
   imgProducto = document.getElementsByClassName("avatarImgItem");
+  content=document.getElementById("content");
   for(let item of imgProducto){
     item.addEventListener("dragstart", moviendoImg);
   }
   avatarCont=document.getElementById("carritoImg");
   avatarCont.addEventListener("dragover", e=>{e.preventDefault()})
   avatarCont.addEventListener("drop", cambiarImg)
-
 }
 
 document.addEventListener("DOMContentLoaded", domCargado)
