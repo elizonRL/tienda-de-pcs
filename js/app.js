0149186227d0e5a078b8dcb01d471583ab1fc07c
elizo <elizo@DESKTOP-0J85MTD>
2023-03-22 14:31:47 -0400

    Funcion Para Limpiar el carrito

--- js/app.js
+++ js/app.js
@@ -3,11 +3,12 @@ var itemImg;
 var arrayItem = [];
 var avatarCont;
 var content;
+var btn;
 function moviendoImg(event){
     itemImg = event.target;
     arrayItem.push(itemImg.src);
     setDatos(arrayItem);
-    console.log(arrayItem);
+    //console.log(arrayItem);
 }
 
 function cambiarImg(event){
@@ -18,16 +19,27 @@ function cambiarImg(event){
     }
 }
 
+function borrarcarrito(event){
+    arrayItem.splice(0, arrayItem.length)
+    cambiarImg();
+    console.log(arrayItem);
+  
+ event.preventDefault();
+ 
+}
+
 function domCargado(){
   imgProducto = document.getElementsByClassName("avatarImgItem");
+  btn = document.getElementById("btn");
+  form = document.getElementById("form");
   content=document.getElementById("content");
   for(let item of imgProducto){
     item.addEventListener("dragstart", moviendoImg);
   }
   avatarCont=document.getElementById("dragable");
-  avatarCont.addEventListener("dragover", e=>{e.preventDefault()})
-  avatarCont.addEventListener("drop", cambiarImg)
-  
+  avatarCont.addEventListener("dragover", e=>{e.preventDefault()});
+  avatarCont.addEventListener("drop", cambiarImg);
+  form.addEventListener('submit', borrarcarrito);
 }
 
 document.addEventListener("DOMContentLoaded", domCargado)
